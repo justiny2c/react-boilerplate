ad5ca4710e477accb16f3fdad826bd90da6016a9
justiny2c <justiny2c@gmail.com>
2020-02-25 11:59:22 -0800

    Working on test

--- app/containers/TodoList/tests/index.test.js
+++ app/containers/TodoList/tests/index.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from 'react-testing-library';
 
-import ListContainer from '../TodoList';
+import { ListContainer } from '../TodoList';
+import TodoList from '../TodoList';
 
+// jest
+describe('<TodoList />', () => {
+  it('renders correctly', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<TodoList />, div);
+  });
+});
+
+// react testing library
 describe('<ListContainer />', () => {
   it('should render an <div> tag', () => {
     const {
